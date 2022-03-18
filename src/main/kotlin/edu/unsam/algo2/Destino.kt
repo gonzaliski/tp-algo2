6b8f50af1f548444873fa942a5ec889fd2ef6750
Luz Cucarella <luz.cucarella97@gmail.com>
2022-03-18 16:36:39 -0300

    Agregamos Destino

---
+++ src/main/kotlin/edu/unsam/algo2/Destino.kt
@@ -0,0 +1,25 @@
+package edu.unsam.algo2
+
+class Destino(
+    var pais: String,
+    var cuidad: String,
+    val costo_base: Double
+) {
+
+    fun esLocal() =
+        pais.lowercase().trim() == "Argentina".lowercase().trim()
+
+    fun costo(usuario: Usuario): Double {
+        var costo: Double = costo_base
+
+        if (!esLocal()) {
+            costo *= 1.2
+        }
+        if (usuario.pais_residencia == pais) {
+            costo -= (0.01 * costo_base) * minOf(usuario.antiguedad(), 15)
+        }
+
+        return costo
+    }
+
+}
