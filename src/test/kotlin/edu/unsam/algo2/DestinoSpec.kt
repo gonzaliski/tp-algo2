1543acfc21f12b557749b3c6c8b7d5d8b0502f5a
Luz Cucarella <luz.cucarella97@gmail.com>
2022-03-28 16:46:14 -0300

    Usar fechas relativas #9

--- src/test/kotlin/edu/unsam/algo2/DestinoSpec.kt
+++ src/test/kotlin/edu/unsam/algo2/DestinoSpec.kt
@@ -10,11 +10,7 @@ internal class DestinoSpec : DescribeSpec({
             nombre = "Pepe",
             apellido = "Rodriguez",
             username = "peper",
-            fechaAlta = LocalDate.of(
-                /* year = */ LocalDate.now().year - 2, // antiguedad es siempre 2
-                /* month = */ 5,
-                /* dayOfMonth = */ 11
-            ),
+            fechaAlta = LocalDate.now().minusYears(2),
             paisResidencia = ""
         )
         describe("Destino local") {
@@ -35,9 +31,9 @@ internal class DestinoSpec : DescribeSpec({
     describe("Usuario con mucha antigüedad (+15) y pais residencia igual a pais destino") {
         val pepe = Usuario(
             nombre = "Pepe",
-            apellido ="Perez",
+            apellido = "Perez",
             username = "peperez",
-            fechaAlta = LocalDate.of(2006, 3, 18),
+            fechaAlta = LocalDate.now().minusYears(16),
             paisResidencia = "Argentina"
         )
         describe("Destino local") {
@@ -53,17 +49,17 @@ internal class DestinoSpec : DescribeSpec({
     }
 
     describe("Usuario con 16 años de antiguedad y pais de residencia Argentina") {
-        val usuario = Usuario("Pepe","Rodriguez", "peper", LocalDate.of(LocalDate.now().year - 16,3,20),"Argentina")
+        val usuario = Usuario("Pepe", "Rodriguez", "peper", LocalDate.now().minusYears(16), "Argentina")
 
         describe("Destino local") {
             val destino = Destino("Argentina", "MDQ", 1000.00)
-            it("Con descuento del 1% por cada año de antiguedad"){
+            it("Con descuento del 1% por cada año de antiguedad") {
                 destino.costo(usuario) shouldBe 850.00 //15% sobre el costo base del destino
             }
         }
 
         describe("Pais de residencia distinto al pais destino") {
-            val destino = Destino("Estados Unidos","New York",5000.00)//reasigno un nuevo destino no local
+            val destino = Destino("Estados Unidos", "New York", 5000.00)//reasigno un nuevo destino no local
             it("Con recargo del 20% por ser destino NO local") {
                 destino.esLocal() shouldBe false
                 destino.costo(usuario) shouldBe 6000.00
@@ -75,9 +71,9 @@ internal class DestinoSpec : DescribeSpec({
     describe("Usuario con mucha antigüedad (+15) con pais residencia distinto a pais destino") {
         val user = Usuario(
             nombre = "Carlos",
-            apellido ="Gomez",
+            apellido = "Gomez",
             username = "cgomez",
-            fechaAlta = LocalDate.of(2003, 11, 25),
+            fechaAlta = LocalDate.now().minusYears(16),
             paisResidencia = "Chile"
         )
         describe("Destino no local") {
@@ -91,28 +87,26 @@ internal class DestinoSpec : DescribeSpec({
             }
         }
     }
-    
-      describe("Test con usuario con mucha antiguedad (+15), pais de residencia igual al pais destino ") {
-        val frank = Usuario( 
-                nombre = "Frank", 
-                apellido = "Arnold", 
-                username = "frarnold", 
-                fechaAlta = LocalDate.of(2003, 2, 5),
-                paisResidencia = "Inglaterra"
-            )
 
-        describe("Test con destino no local"){
+    describe("Test con usuario con mucha antiguedad (+15), pais de residencia igual al pais destino ") {
+        val frank = Usuario(
+            nombre = "Frank",
+            apellido = "Arnold",
+            username = "frarnold",
+            fechaAlta = LocalDate.now().minusYears(16),
+            paisResidencia = "Inglaterra"
+        )
+
+        describe("Test con destino no local") {
             val londres = Destino(
                 pais = "Inglaterra",
                 ciudad = "Londres",
                 costoBase = 60000.0
             )
-            it("20% más por no ser destino local y descuento por antiguedad (15%), costo de 63000"){
+            it("20% más por no ser destino local y descuento por antiguedad (15%), costo de 63000") {
                 londres.costo(frank) shouldBe 63000.0
             }
         }
-
-        
     }
 })
 
