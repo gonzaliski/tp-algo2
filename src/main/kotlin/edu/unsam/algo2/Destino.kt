1fd60ed092b2866678c135f44311e0cd02b2eb9b
Gon <gon.mendez13@gmail.com>
2022-03-28 17:13:34 -0300

    Desacoplamos la funcion costo en Destino

--- src/main/kotlin/edu/unsam/algo2/Destino.kt
+++ src/main/kotlin/edu/unsam/algo2/Destino.kt
@@ -6,20 +6,14 @@ class Destino(
     val costoBase: Double
 ) {
 
+
     fun esLocal() =
         pais.lowercase().trim() == "Argentina".lowercase().trim()
 
     fun costo(usuario: Usuario): Double {
         var costo: Double = costoBase
-
-        if (!esLocal()) {
-            costo *= 1.2
-        }
-        if (usuario.paisResidencia == pais) {
-            costo -= (0.01 * costoBase) * minOf(usuario.antiguedad(), 15)
-        }
-
-        return costo
+        return costo + recargo(costo) - descuentoPara(usuario, costo)
     }
-
+    fun recargo(costo:Double):Double = if (!esLocal())  costo *0.2 else 0.0
+    fun descuentoPara(usuario: Usuario, costo: Double):Double = if (usuario.paisResidencia == pais)  (0.01 * costoBase) * minOf(usuario.antiguedad(), 15) else 0.0
 }
