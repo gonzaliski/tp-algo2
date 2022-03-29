36e4b6ae1639f1837987670a1dcd0df21e235e3e
Luz Cucarella <luz.cucarella97@gmail.com>
2022-03-29 17:47:16 -0300

    Usuario chequea si puede realizar un itinerario
    Agregamos la clase Criterio

--- src/main/kotlin/edu/unsam/algo2/Usuario.kt
+++ src/main/kotlin/edu/unsam/algo2/Usuario.kt
@@ -9,7 +9,8 @@ class Usuario(
     val username: String,
     val fechaAlta: LocalDate,
     var paisResidencia: String,
-    var diasDisponibles: Int
+    var diasDisponibles: Int,
+    var criterio: Criterio
 ) {
 
     val amigos: MutableList<Usuario> = mutableListOf()
@@ -22,4 +23,50 @@ class Usuario(
 
     fun puedePuntuar(itinerario: Itinerario): Boolean = itinerario.creador != this && conoce(itinerario.destino)
 
+    fun puedeRealizar(itinerario: Itinerario): Boolean =
+        diasDisponibles >= itinerario.cantidadDias() && criterio.puedeRealizar(itinerario, this)
+
+    fun esDestinoCaro(destinoItinerario: Destino): Boolean =
+        destinosDeseados.maxOf { destino -> destino.costo(this) } > destinoItinerario.costo(this)
+}
+
+sealed class Criterio() {
+    abstract fun puedeRealizar(itinerario: Itinerario, usuario: Usuario): Boolean
+
+    object Relajado : Criterio() {
+        override fun puedeRealizar(itinerario: Itinerario, usuario: Usuario): Boolean = true
+    }
+
+    object Precavido : Criterio() {
+        override fun puedeRealizar(itinerario: Itinerario, usuario: Usuario): Boolean {
+            return usuario.conoce(itinerario.destino) || usuario.amigos.any { it.conoce(itinerario.destino) }
+        }
+    }
+
+    object Localista : Criterio() {
+        override fun puedeRealizar(itinerario: Itinerario, usuario: Usuario): Boolean {
+            return itinerario.destino.esLocal() // O igual a Argentina?
+        }
+    }
+
+    object Soniador : Criterio() {
+        override fun puedeRealizar(itinerario: Itinerario, usuario: Usuario): Boolean {
+            return usuario.destinosDeseados.contains(itinerario.destino) || usuario.esDestinoCaro(itinerario.destino)
+        }
+    }
+
+    object Activo : Criterio() {
+        override fun puedeRealizar(itinerario: Itinerario, usuario: Usuario): Boolean {
+            return itinerario.tieneActividadesTodosLosDias()
+        }
+    }
+
+    class Exigente(var porcentaje: Double, var dificultad: Actividad.Dificultad) : Criterio() {
+        override fun puedeRealizar(
+            itinerario: Itinerario,
+            usuario: Usuario
+        ): Boolean {
+            return itinerario.porcentajeDeActividades(dificultad) >= porcentaje
+        }
+    }
 }
