b352b267d1b283c6926a5d5e4d89017d65c29e59
adanrdrgz <aj.rdrgz444@gmail.com>
2022-04-02 17:50:38 -0300

    Validaciones Actividad

--- src/main/kotlin/edu/unsam/algo2/Actividad.kt
+++ src/main/kotlin/edu/unsam/algo2/Actividad.kt
@@ -5,45 +5,27 @@ import java.time.temporal.ChronoUnit
 
 class Actividad(
 
-    var dificultad: Dificultad = Dificultad.BAJA
+    var dificultad: Dificultad = Dificultad.BAJA,
+    var descripcion: String,
+    var inicio: LocalTime,
+    var fin: LocalTime,
+    var costo: Double
 ) {
-    var descripcion: String = ""
-        get() = field
-        set(value){
-            if(!value.isEmpty()){
-                field = value
-            }else{
-                throw error("Descripcion de la actividad ser nulo o vacio")
-            }
+
+    init {
+        require( descripcion.isNotBlank() ){
+            "Descripcion de la actividad ser nulo o vacio"
         }
 
-    var inicio: LocalTime = LocalTime.now()
-        get() = field
-        set(value){
-            if(value < fin){
-                field = value
-            }else{
-                throw error("El horario de inicio debe ser menor que el de fin")
-            }
+        require( inicio < fin){
+            "El horario de inicio debe ser menor que el de fin"
         }
-    var fin: LocalTime = LocalTime.now()
-        get() = field
-        set(value){
-            if(value > inicio){
-                field = value
-            }else{
-                throw error("El horario de fin debe ser mayor que el de inicio")
-            }
+
+        require( costo >= 0.0 ){
+            "Costo no puede ser menor a 0"
         }
-    var costo: Double = 0.0
-        get() = field
-        set(value){
-            if(value >= 0){
-                field = value
-            }else{
-                throw error("Costo no puede ser menor a 0")
-            }
-        }
+    }
+
     fun duracion() = ChronoUnit.MINUTES.between(fin, inicio)
 
     enum class Dificultad(valor: Int) {
