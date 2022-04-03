5d2f2098e87bc64a874ddb80793eb44f6ecd7cd8
Luz Cucarella <luz.cucarella97@gmail.com>
2022-04-03 14:58:42 -0300

    Arreglo ValidacionSpec
    Habían parametros que debían estar en el constructor del objecto pero estaban dentro de un apply. El apply es llamado luego de inicializar el objeto, por lo que no sirve para inicializar parametros del constructor, si no para modificar valores inmediatamente después del init() (entre otras cosas)

--- src/test/kotlin/edu/unsam/algo2/ValidacionSpec.kt
+++ src/test/kotlin/edu/unsam/algo2/ValidacionSpec.kt
@@ -5,54 +5,52 @@ import io.kotest.matchers.shouldBe
 import java.time.LocalDate
 
 internal class ValidacionSpec : DescribeSpec({
-    describe("Validacion de destino"){
-        it("No tira error xD"){
-            val destino = Destino().apply{
-                pais = "Argentina"
-                ciudad = "Runcuncun"
+    describe("Validacion de destino") {
+        it("No tira error xD") {
+            val destino = Destino(
+                pais = "Argentina",
+                ciudad = "Runcuncun",
                 costoBase = 25_000.0
-            }
+            )
             destino.pais shouldBe "Argentina"
         }
     }
-    describe("Funcion init a ver si labura"){
-        it("Nombre deberia ser lo que le pasemos"){
-            val destino = Destino().apply{
-                pais = "Argentina"
-                ciudad = "Runcuncun"
+    describe("Funcion init a ver si labura") {
+        it("Nombre deberia ser lo que le pasemos") {
+            val destino = Destino(
+                pais = "Argentina",
+                ciudad = "Runcuncun",
                 costoBase = 25_000.0
-            }
+            )
             val usuario = Usuario(
                 nombre = "Peperoni",
                 fechaAlta = LocalDate.now(),
                 diasDisponibles = 15,
-                criterio = Criterio.Relajado
-            ).apply{
-                apellido = "Peter"
-                username = "ppeter"
+                criterio = Criterio.Relajado,
+                apellido = "Peter",
+                username = "ppeter",
                 paisResidencia = "Venezuela"
-            }
+            )
             destino.pais shouldBe "Argentina"
         }
     }
-    describe("Test de fecha de alta, dias disponibles y destinos deseados "){
-        it("Nombre deberia ser lo que le pasemos"){
-            val destino = Destino().apply{
-                pais = "Argentina"
-                ciudad = "Runcuncun"
+    describe("Test de fecha de alta, dias disponibles y destinos deseados ") {
+        it("Nombre deberia ser lo que le pasemos") {
+            val destino = Destino(
+                pais = "Argentina",
+                ciudad = "Runcuncun",
                 costoBase = 25_000.0
-            }
+            )
             val usuario = Usuario(
                 nombre = "Peperoni",
                 fechaAlta = LocalDate.now().plusDays(3),
                 diasDisponibles = 0,
-                criterio = Criterio.Relajado
-            ).apply{
-                apellido = "Peter"
-                username = "ppeter"
+                criterio = Criterio.Relajado,
+                apellido = "Peter",
+                username = "ppeter",
                 paisResidencia = "Venezuela"
+            ).apply {
                 destinosDeseados = mutableListOf()
-
             }
             destino.pais shouldBe "Argentina"
         }
