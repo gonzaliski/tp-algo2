bee6867746bea7c740bce500c96d1ca66b00ce2b
Luz Cucarella <luz.cucarella97@gmail.com>
2022-04-03 15:58:45 -0300

    Arreglo tests de validaciones
    Además les hago mejoras para testear por separado cada validacion y chequear que realmente fallen como deben

--- src/test/kotlin/edu/unsam/algo2/ValidacionSpec.kt
+++ src/test/kotlin/edu/unsam/algo2/ValidacionSpec.kt
@@ -1,58 +1,83 @@
 package edu.unsam.algo2
 
+import io.kotest.assertions.throwables.shouldNotThrowAny
+import io.kotest.assertions.throwables.shouldThrowMessage
 import io.kotest.core.spec.style.DescribeSpec
-import io.kotest.matchers.shouldBe
 import java.time.LocalDate
 
 internal class ValidacionSpec : DescribeSpec({
     describe("Validacion de destino") {
         it("No tira error xD") {
-            val destino = Destino(
-                pais = "Argentina",
-                ciudad = "Runcuncun",
-                costoBase = 25_000.0
-            )
-            destino.pais shouldBe "Argentina"
-        }
-    }
-    describe("Funcion init a ver si labura") {
-        it("Nombre deberia ser lo que le pasemos") {
-            val destino = Destino(
-                pais = "Argentina",
-                ciudad = "Runcuncun",
-                costoBase = 25_000.0
-            )
-            val usuario = Usuario(
-                nombre = "Peperoni",
-                fechaAlta = LocalDate.now(),
-                diasDisponibles = 15,
-                criterio = Criterio.Relajado,
-                apellido = "Peter",
-                username = "ppeter",
-                paisResidencia = "Venezuela"
-            )
-            destino.pais shouldBe "Argentina"
-        }
-    }
-    describe("Test de fecha de alta, dias disponibles y destinos deseados ") {
-        it("Nombre deberia ser lo que le pasemos") {
-            val destino = Destino(
-                pais = "Argentina",
-                ciudad = "Runcuncun",
-                costoBase = 25_000.0
-            )
-            val usuario = Usuario(
-                nombre = "Peperoni",
-                fechaAlta = LocalDate.now().plusDays(3),
-                diasDisponibles = 0,
-                criterio = Criterio.Relajado,
-                apellido = "Peter",
-                username = "ppeter",
-                paisResidencia = "Venezuela"
-            ).apply {
-                destinosDeseados = mutableListOf()
+            shouldNotThrowAny {
+                Destino(
+                    pais = "Argentina",
+                    ciudad = "Runcuncun",
+                    costoBase = 25_000.0
+                )
             }
-            destino.pais shouldBe "Argentina"
+        }
+    }
+    describe("Validaciones en Usuario") {
+        val destino = Destino(
+            pais = "Argentina",
+            ciudad = "Runcuncun",
+            costoBase = 25_000.0
+        )
+        it("Usuario bien creado no falla") {
+            shouldNotThrowAny {
+                Usuario(
+                    nombre = "Peperoni",
+                    fechaAlta = LocalDate.now(),
+                    diasDisponibles = 15,
+                    criterio = Criterio.Relajado,
+                    apellido = "Peter",
+                    username = "ppeter",
+                    paisResidencia = "Venezuela",
+                    destinosDeseados = mutableListOf(destino)
+                )
+            }
+        }
+        it("Usuario falla con fecha de alta en el futuro") {
+            shouldThrowMessage("La fecha de alta no puede ser posterior a la del día.") {
+                Usuario(
+                    nombre = "Peperoni",
+                    fechaAlta = LocalDate.now().plusDays(3),
+                    diasDisponibles = 2,
+                    criterio = Criterio.Relajado,
+                    apellido = "Peter",
+                    username = "ppeter",
+                    paisResidencia = "Venezuela",
+                    destinosDeseados = mutableListOf(destino)
+                )
+            }
+        }
+        it("Usuario falla sin destinos deseados") {
+            shouldThrowMessage("Todos los usuarios deben tener al menos un destino deseado.") {
+                Usuario(
+                    nombre = "Peperoni",
+                    fechaAlta = LocalDate.now(),
+                    diasDisponibles = 2,
+                    criterio = Criterio.Relajado,
+                    apellido = "Peter",
+                    username = "ppeter",
+                    paisResidencia = "Venezuela",
+                    destinosDeseados = mutableListOf()
+                )
+            }
+        }
+        it("Usuario falla si no tiene dias para viajar") {
+            shouldThrowMessage("Los días para viajar, deben ser mayores a cero.") {
+                Usuario(
+                    nombre = "Peperoni",
+                    fechaAlta = LocalDate.now(),
+                    diasDisponibles = 0,
+                    criterio = Criterio.Relajado,
+                    apellido = "Peter",
+                    username = "ppeter",
+                    paisResidencia = "Venezuela",
+                    destinosDeseados = mutableListOf(destino)
+                )
+            }
         }
     }
 })
