8fabe8831ee94931acbee6323ebd9052c49d8912
Luz Cucarella <luz.cucarella97@gmail.com>
2022-04-10 20:44:03 -0300

    Arreglo tests de Usuario reutilizando codigo
    Habian varios destinos que se repetian varias veces al igual que algunas
    activiadades.
    Los puse en variables para que sean comunes a todos los tests y poder
    reutilizarlos.

--- src/test/kotlin/edu/unsam/algo2/UsuarioSpec.kt
+++ src/test/kotlin/edu/unsam/algo2/UsuarioSpec.kt
@@ -1,14 +1,40 @@
-
 package edu.unsam.algo2
 
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.booleans.shouldBeFalse
 import io.kotest.matchers.booleans.shouldBeTrue
-import io.kotest.matchers.shouldBe
 import java.time.LocalDate
 import java.time.LocalTime
 
-class UsuarioSpec: DescribeSpec ({
+class UsuarioSpec : DescribeSpec({
+    val actividadesBasicas = mutableListOf(
+        Actividad(
+            dificultad = Actividad.Dificultad.BAJA,
+            descripcion = "asdjasdja",
+            inicio = LocalTime.of(12, 30, 0, 0),
+            fin = LocalTime.of(14, 30, 0, 0),
+            costo = 0.0
+
+        ), Actividad(
+            dificultad = Actividad.Dificultad.MEDIA,
+            descripcion = "asdjasdjsa",
+            inicio = LocalTime.of(15, 30, 0, 0),
+            fin = LocalTime.of(17, 30, 0, 0),
+            costo = 0.0
+        )
+    )
+    val roma = Destino(
+        pais = "Italia", ciudad = "Roma", costoBase = 3.0
+    )
+    val cucurun = Destino(
+        pais = "Runcuncun", ciudad = "Cucurun", costoBase = 3.0
+    )
+    val santiagoDeChile = Destino(
+        pais = "Chile", ciudad = "Santiago", costoBase = 3.0
+    )
+    val laPaz = Destino(
+        pais = "Bolivia", ciudad = "La Paz", costoBase = 3.0
+    )
     val usuario = Usuario(
         nombre = "Pepe",
         apellido = "Peter",
@@ -18,157 +44,56 @@ class UsuarioSpec: DescribeSpec ({
         diasDisponibles = 15,
         criterio = Relajado,
         destinosDeseados = mutableListOf(
-            Destino(
-                pais = "Italia",
-                ciudad = "Roma",
-                costoBase = 3.0
-            ),
-
-            Destino(
-                pais ="Runcuncun",
-                ciudad = "Cucurun",
-                costoBase = 3.0
-            )
+            roma, cucurun
         )
     ).apply {
         destinosVisitados = mutableListOf(
-            Destino(
-                pais = "Chile",
-                ciudad = "Santiago",
-                costoBase = 3.0
-            )
+            santiagoDeChile
         )
     }
+    val otroUsuario = Usuario(
+        nombre = "Carlos",
+        apellido = "Gomez",
+        username = "cgomez",
+        fechaAlta = LocalDate.now().minusYears(16),
+        paisResidencia = "Chile",
+        diasDisponibles = 1,
+        criterio = Relajado,
+        destinosDeseados = mutableListOf(
+            roma, cucurun
+        )
+    )
 
-
-    it("Puede puntuar itinerario de otro cuyo destino conoce"){
+    it("Puede puntuar itinerario de otro cuyo destino conoce") {
         val itinerario = Itinerario(
-            creador = Usuario(
-                nombre = "Carlos",
-                apellido = "Gomez",
-                username = "cgomez",
-                fechaAlta = LocalDate.now().minusYears(16),
-                paisResidencia = "Chile",
-                diasDisponibles = 1,
-                criterio = Relajado,
-                destinosDeseados = mutableListOf(
-                    Destino(
-                        pais = "Italia",
-                        ciudad = "Roma",
-                        costoBase = 3.0
-                    ),
-
-                    Destino(
-                        pais ="Runcuncun",
-                        ciudad = "Cucurun",
-                        costoBase = 3.0
-                    )
-                )
-            ),
+            creador = otroUsuario,
             destino = usuario.destinosDeseados.first(),
             dias = mutableListOf(
-                Itinerario.DiaDeItinerario(
-                    mutableListOf(
-                        Actividad(
-                            dificultad = Actividad.Dificultad.BAJA,
-                            descripcion = "asdjasdja",
-                            inicio = LocalTime.of(12,30,0,0),
-                            fin = LocalTime.of(14,30,0,0),
-                            costo = 0.0
-
-                        ),
-                        Actividad(
-                            dificultad = Actividad.Dificultad.MEDIA,
-                            descripcion = "asdjasdjsa",
-                            inicio = LocalTime.of(15,30,0,0),
-                            fin = LocalTime.of(17,30,0,0),
-                            costo = 0.0                    )
-                    )
-                )
+                Itinerario.DiaDeItinerario(actividadesBasicas)
             )
         )
 
         usuario.puedePuntuar(itinerario).shouldBeTrue()
-
     }
 
-    it("No puede puntuar porque es el creador"){
+    it("No puede puntuar porque es el creador") {
         val itinerario = Itinerario(
             creador = usuario,
             destino = usuario.destinosDeseados.first(),
             dias = mutableListOf(
-                Itinerario.DiaDeItinerario(
-                    mutableListOf(
-                        Actividad(
-                            dificultad = Actividad.Dificultad.BAJA,
-                            descripcion = "asdjasdja",
-                            inicio = LocalTime.of(12,30,0,0),
-                            fin = LocalTime.of(14,30,0,0),
-                            costo = 0.0
-                        ),
-                        Actividad(
-                            dificultad = Actividad.Dificultad.BAJA,
-                            descripcion = "asdjasdja",
-                            inicio = LocalTime.of(15,30,0,0),
-                            fin = LocalTime.of(17,30,0,0),
-                            costo = 0.0
-                        )
-                    )
-                )
+                Itinerario.DiaDeItinerario(actividadesBasicas)
             )
         )
 
         usuario.puedePuntuar(itinerario).shouldBeFalse()
     }
 
-    it("No puede puntuar porque no conoce el destino"){
+    it("No puede puntuar porque no conoce el destino") {
         val itinerario = Itinerario(
-            creador = Usuario(
-                nombre = "Carlos",
-                apellido = "Gomez",
-                username = "cgomez",
-                fechaAlta = LocalDate.now().minusYears(16),
-                paisResidencia = "Chile",
-                diasDisponibles = 1,
-                criterio = Relajado,
-                destinosDeseados = mutableListOf(
-                    Destino(
-                        pais = "Italia",
-                        ciudad = "Roma",
-                        costoBase = 3.0
-                    ),
-
-                    Destino(
-                        pais ="Runcuncun",
-                        ciudad = "Cucurun",
-                        costoBase = 3.0
-                    )
-                )
-            ),
-            destino = Destino(
-                pais = "Bolivia",
-                ciudad = "La Paz",
-                costoBase = 3.0
-            ),
+            creador = otroUsuario,
+            destino = laPaz,
             dias = mutableListOf(
-                Itinerario.DiaDeItinerario(
-                    mutableListOf(
-                        Actividad(
-                            dificultad = Actividad.Dificultad.BAJA,
-                            descripcion = "asdjasdja",
-                            inicio = LocalTime.of(12,30,0,0),
-                            fin = LocalTime.of(14,30,0,0),
-                            costo = 0.0
-                        ),
-                        Actividad(
-                            dificultad = Actividad.Dificultad.BAJA,
-                            descripcion = "asdjasdja",
-                            inicio = LocalTime.of(15,30,0,0),
-                            fin = LocalTime.of(17,30,0,0),
-                            costo = 0.0
-                        )
-                    )
-                )
+                Itinerario.DiaDeItinerario(actividadesBasicas)
             )
         )
 
