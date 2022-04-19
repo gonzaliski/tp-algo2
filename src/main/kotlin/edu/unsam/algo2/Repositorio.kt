6f1ab2ecfcb01a0c838494cf18fff79edd1e6a6e
Luz Cucarella <luz.cucarella97@gmail.com>
2022-04-18 22:07:00 -0300

    Change: Cambio de nombre de NEXT_ID y TgetById
    'NEXT_ID' al ser un atributo debería empezar en minuscula y ser camelCase. Además 'nextID' no debería poder ser modificada fuera del repositorio por lo que le asignamos un  privado. Por otro lado, la funcion 'TgetById' debía llamarse 'getById'.

--- src/main/kotlin/edu/unsam/algo2/Repositorio.kt
+++ src/main/kotlin/edu/unsam/algo2/Repositorio.kt
@@ -3,13 +3,14 @@ package edu.unsam.algo2
 // val repo = Repositorio<Usuario>()
 
 class Repositorio<T : Identificable> {
-    private var NEXT_ID = 0
+    var nextID = 0
+        private set
     val elementos: MutableList<T> = mutableListOf()
 
     /**Agrega un nuevo objeto a la colección, y le asigna un identificador único (id).
      * El identificador puede ser autoincremental para evitar que se repita.*/
     fun create(elemento: T) {
-        elemento.id = NEXT_ID++
+        elemento.id = nextID++
         elementos.add(elemento)
     }
 
@@ -21,7 +22,7 @@ class Repositorio<T : Identificable> {
     /**Modifica el objeto dentro de la colección.
      * De no existir el objeto buscado, es decir, un objeto con ese id, se debe lanzar una excepción.*/
     fun update(elemento: T){
-        val elementoEncontrado = elemento.id?.let { TgetById(it) }
+        val elementoEncontrado = elemento.id?.let { getById(it) }
             ?: throw java.lang.NullPointerException("El elemento buscado no ha sido encontrado")
 
         //TODO: preguntar que es lo que hay que modificar en caso de que el objeto se encuentre en la coleccion.
@@ -29,7 +30,7 @@ class Repositorio<T : Identificable> {
 
 
     /**Retorna el objeto cuyo id sea el recibido como parámetro.*/
-    fun TgetById(id: Int):T?{
+    fun getById(id: Int):T?{
         return elementos.find{id == it.id}
     }
 
