e313f0b3d3ab0e71360f4e452639346c9712f52e
Daiana Micaela Castro <daianamcastro153@gmail.com>
2022-04-21 14:49:02 -0300

    Realizo el cambio del #40

--- src/main/kotlin/edu/unsam/algo2/PreferenciaDeVehiculo.kt
+++ src/main/kotlin/edu/unsam/algo2/PreferenciaDeVehiculo.kt
@@ -1,38 +1,38 @@
 package edu.unsam.algo2
 
-interface VehiculoPreferencia {
+interface PreferenciaDeVehiculo {
     fun leGusta(vehiculo: Vehiculo): Boolean
 }
 
-object Neofilo : VehiculoPreferencia {
+object Neofilo : PreferenciaDeVehiculo {
     override fun leGusta(vehiculo: Vehiculo): Boolean = vehiculo.antiguedad() < 2
 }
 
 /** solo quiere vehículos fabricados en años pares*/
-object Supersticioso : VehiculoPreferencia {
+object Supersticioso : PreferenciaDeVehiculo {
     override fun leGusta(vehiculo: Vehiculo): Boolean = vehiculo.anioFabricacion.isEven()
 }
 
 /** le gustan los vehículos cuya inicial de la marca, coincida con la inicial del modelo. */
-object Caprichoso : VehiculoPreferencia {
+object Caprichoso : PreferenciaDeVehiculo {
     override fun leGusta(vehiculo: Vehiculo): Boolean =
         vehiculo.marca.trim().first() == vehiculo.modelo.trim().first() // TODO: Delegar a Vehiculo
     // vehiculo.coincidenInciales()
 }
 
 /** siempre que el vehículo sea de una marca específica.*/
-class Selectivo(private val marca: String) : VehiculoPreferencia {
+class Selectivo(private val marca: String) : PreferenciaDeVehiculo {
     override fun leGusta(vehiculo: Vehiculo): Boolean = vehiculo.marca == marca
 }
 
 /** quiere que tenga el kilometraje libre*/
-object SinLimite : VehiculoPreferencia {
+object SinLimite : PreferenciaDeVehiculo {
     override fun leGusta(vehiculo: Vehiculo): Boolean = vehiculo.kilometrajeLibre
 }
 
 /** es la combinación de 2 ó más, de las formas de determinar mencionadas antes, en cuyo caso, para que le
  * guste un vehículo se tienen que cumplir todas las condiciones impuestas por las distintas formas.*/
-class Combinado(private val preferencias: MutableList<VehiculoPreferencia>) : VehiculoPreferencia {
+class Combinado(private val preferencias: MutableList<PreferenciaDeVehiculo>) : PreferenciaDeVehiculo {
     override fun leGusta(vehiculo: Vehiculo): Boolean =
         preferencias.all { preferencia -> preferencia.leGusta(vehiculo) }
 }
--- src/main/kotlin/edu/unsam/algo2/Usuario.kt
+++ src/main/kotlin/edu/unsam/algo2/Usuario.kt
@@ -12,14 +12,14 @@ class Usuario(
     var diasDisponibles: Int,
     var criterio: Criterio,
     var destinosDeseados: MutableList<Destino>,
-    var vehiculoPreferencia: VehiculoPreferencia
+    var vehiculoPreferencia: PreferenciaDeVehiculo
 ): Identidad {
     val amigos: MutableList<Usuario> = mutableListOf()
     var destinosVisitados: MutableList<Destino> = mutableListOf()
     override var id: Int? = null
 
     fun leGusta(vehiculo: Vehiculo) = vehiculoPreferencia.leGusta(vehiculo)
-    
+
     init {
         require(nombre.isNotBlank()) {
             "El nombre del usuario no puede ser nulo o vacio"
